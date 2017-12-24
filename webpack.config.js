bc0d9ef0ec71bf42ecb4197485890bfd29cfec71
Duc Dao <huuduc.uneti@gmail.com>
2017-12-24 20:57:14 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+
+  output: {
+    path: __dirname + '/build',
+    publicPath: '/',
+    filename: 'app.js'
+  },
+
+  devServer: {
+    contentBase: './build',
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ]
+};
