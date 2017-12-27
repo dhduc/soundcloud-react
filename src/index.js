4a3e12a263749364f6bdb347599602c1a289b05a
Duc Dao <huuduc.uneti@gmail.com>
2017-12-27 20:10:32 +0700

    Add react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Soundcloud client!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TrackList from './components/TrackList';
+
+const tracks = [
+    {
+        id: 1,
+        title: 'Shape of you'
+    },
+    {
+        id: 2,
+        title: 'That what I like'
+    }
+];
+
+ReactDOM.render(
+    < TrackList tracks={tracks} />,
+    document.getElementById('app')
+);
