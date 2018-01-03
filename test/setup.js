ec690baa414bfc8ae4b143130b614e324321ac3c
Duc Dao <ducdh1@smartosc.com>
2018-01-03 11:19:25 +0700

    Add npm test

---
+++ test/setup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+
+const document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const window = document.defaultView;
+
+global.document = document;
+global.window = window;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
+global.React = React;
+global.expect = expect;
+
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
